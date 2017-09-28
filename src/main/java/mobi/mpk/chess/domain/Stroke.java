ec2549301a0e849f1da5e7f26e139133e5ad33d4
EvilGenoM <evmezentsev@gmail.com>
2017-09-28 18:12:13 +0300

    Add exception in the stroke and add method find figure on the board

--- src/main/java/mobi/mpk/chess/domain/Stroke.java
+++ src/main/java/mobi/mpk/chess/domain/Stroke.java
@@ -1,7 +1,9 @@
 package mobi.mpk.chess.domain;
 
 import mobi.mpk.chess.domain.exception.CellCorrectException;
+import mobi.mpk.chess.domain.exception.FigureNotFindException;
 import mobi.mpk.chess.domain.exception.StrokeCorrectException;
+import mobi.mpk.chess.domain.figure.Figure;
 
 import java.util.Date;
 
@@ -10,13 +12,15 @@ public class Stroke {
 
     private Cell from;
     private Cell to;
+    private Figure figure;
 
     private Date time;
 
-    public Stroke(Cell from, Cell to){
+    public Stroke(Cell from, Cell to, Figure figure) {
 
         this.from = from;
         this.to = to;
+        this.figure = figure;
 
     }
 
@@ -25,16 +29,42 @@ public class Stroke {
 
         String[] coordCell = stroke.split(" ");
 
-        if(coordCell.length != 2){
+        if (coordCell.length != 2) {
             throw new StrokeCorrectException();
         }
 
-        if(coordCell[0] != null && coordCell[1] != null){
+        if (coordCell[0] != null && coordCell[1] != null) {
             this.from = createCell(coordCell[0]);
             this.to = createCell(coordCell[1]);
         }
 
         this.time = new Date();
+
+    }
+
+    private Cell createCell(String coordCell) throws CellCorrectException, StrokeCorrectException {
+
+        if (coordCell.length() > 2) {
+            throw new StrokeCorrectException();
+        }
+
+        char xCoordCell = coordCell.charAt(0);
+        int yCoordCell = Character.getNumericValue(coordCell.charAt(1));
+
+        return new Cell(xCoordCell, yCoordCell);
+
+    }
+
+    public void findFigure(Board board) throws FigureNotFindException {
+
+        Cell from = board.getCell(this.from);
+        Figure figure = from.getFigure();
+
+        if(figure == null){
+            throw new FigureNotFindException();
+        } else {
+            this.figure = figure;
+        }
 
     }
 
@@ -46,18 +76,8 @@ public class Stroke {
         return to;
     }
 
-    private Cell createCell(String coordCell) throws CellCorrectException, StrokeCorrectException {
 
-        if(coordCell.length() > 2){
-            throw new StrokeCorrectException();
-        }
-
-        char xCoordCell = coordCell.charAt(0);
-        int yCoordCell = Character.getNumericValue(coordCell.charAt(1));
-
-        return new Cell(xCoordCell, yCoordCell);
-
+    public Figure getFigure() {
+        return figure;
     }
-
-
 }
