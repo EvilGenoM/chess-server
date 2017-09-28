f4a1aed9efad35bc78bbe6b0b91577cb17a4232f
EvilGenoM <evmezentsev@gmail.com>
2017-09-28 23:31:37 +0300

    Add obstacle for Knight

--- src/main/java/mobi/mpk/chess/domain/rules/rule/obstacle/move/ObstacleMoveKnightRule.java
+++ src/main/java/mobi/mpk/chess/domain/rules/rule/obstacle/move/ObstacleMoveKnightRule.java
@@ -1,4 +1,33 @@
 package mobi.mpk.chess.domain.rules.rule.obstacle.move;
 
-public class ObstacleMoveKnightRule {
+import mobi.mpk.chess.domain.Board;
+import mobi.mpk.chess.domain.Cell;
+import mobi.mpk.chess.domain.Stroke;
+import mobi.mpk.chess.domain.figure.Figure;
+import mobi.mpk.chess.domain.figure.Knight;
+
+public class ObstacleMoveKnightRule implements ObstacleMoveRule {
+
+    @Override
+    public boolean checkRule(Stroke stroke, Board board) {
+
+        Cell to = stroke.getTo();
+        to = board.getCell(to);
+
+        Figure figure = stroke.getFigure();
+
+        if(!figure.equals(new Knight())){
+            return false;
+        }
+
+        if(to.getFigure() == null){
+            return true;
+        } else if(to.getFigure().getColor() != figure.getColor()){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
