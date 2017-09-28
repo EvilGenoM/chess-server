c17a8337d4eddfc7a16778d5918a93934d3c5493
EvilGenoM <evmezentsev@gmail.com>
2017-09-28 23:31:58 +0300

    Add test for ObstacleMoveKnightRule

--- src/test/java/obstaclemove/TestObstacleMoveKnightRule.java
+++ src/test/java/obstaclemove/TestObstacleMoveKnightRule.java
@@ -1,4 +1,105 @@
 package obstaclemove;
 
+import mobi.mpk.chess.domain.Board;
+import mobi.mpk.chess.domain.Cell;
+import mobi.mpk.chess.domain.Color;
+import mobi.mpk.chess.domain.Stroke;
+import mobi.mpk.chess.domain.exception.CellCorrectException;
+import mobi.mpk.chess.domain.exception.FigureNotFindException;
+import mobi.mpk.chess.domain.exception.StrokeCorrectException;
+import mobi.mpk.chess.domain.figure.Bishop;
+import mobi.mpk.chess.domain.figure.Knight;
+import mobi.mpk.chess.domain.figure.Pawn;
+import mobi.mpk.chess.domain.figure.Queen;
+import mobi.mpk.chess.domain.rules.rule.obstacle.move.ObstacleMoveDiagonalRule;
+import mobi.mpk.chess.domain.rules.rule.obstacle.move.ObstacleMoveKnightRule;
+import mobi.mpk.chess.domain.rules.rule.obstacle.move.ObstacleMoveRule;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestObstacleMoveKnightRule {
+
+    private Board board;
+
+    @Before
+    public void before() throws CellCorrectException {
+        board = new Board();
+        Cell cell = board.getCell('e',4);
+        cell.setFigure(new Knight(Color.white));
+
+        cell = board.getCell('b', 1);
+        cell.setFigure(new Queen(Color.white));
+
+        cell = board.getCell('h', 1);
+        cell.setFigure(new Pawn(Color.black));
+    }
+
+    @Test
+    public void testE4B1(){
+
+        Stroke stroke = null;
+        try {
+            stroke = new Stroke("e4 b1");
+            stroke.findFigure(board);
+
+            ObstacleMoveRule rule = new ObstacleMoveKnightRule();
+            boolean result = rule.checkRule(stroke, board);
+
+            assertEquals(result, false);
+        } catch (CellCorrectException e) {
+            e.printStackTrace();
+        } catch (StrokeCorrectException e) {
+            e.printStackTrace();
+        } catch (FigureNotFindException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testE4H1(){
+
+        Stroke stroke = null;
+        try {
+            stroke = new Stroke("e4 h1");
+            stroke.findFigure(board);
+
+            ObstacleMoveRule rule = new ObstacleMoveKnightRule();
+            boolean result = rule.checkRule(stroke, board);
+
+            assertEquals(result, true);
+        } catch (CellCorrectException e) {
+            e.printStackTrace();
+        } catch (StrokeCorrectException e) {
+            e.printStackTrace();
+        } catch (FigureNotFindException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testB1E4(){
+
+        Stroke stroke = null;
+        try {
+            stroke = new Stroke("b1 e4");
+            stroke.findFigure(board);
+
+            ObstacleMoveRule rule = new ObstacleMoveKnightRule();
+            boolean result = rule.checkRule(stroke, board);
+
+            assertEquals(result, false);
+        } catch (CellCorrectException e) {
+            e.printStackTrace();
+        } catch (StrokeCorrectException e) {
+            e.printStackTrace();
+        } catch (FigureNotFindException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
