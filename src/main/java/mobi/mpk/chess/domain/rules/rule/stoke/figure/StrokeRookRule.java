88c846caf9b2cf4fb9393914f99fa49fb7db5db3
EvilGenoM <evmezentsev@gmail.com>
2017-09-28 11:05:19 +0300

    Add strokeRule for Rook

--- src/main/java/mobi/mpk/chess/domain/rules/rule/stoke/figure/StrokeRookRule.java
+++ src/main/java/mobi/mpk/chess/domain/rules/rule/stoke/figure/StrokeRookRule.java
@@ -1,5 +1,42 @@
 package mobi.mpk.chess.domain.rules.rule.stoke.figure;
 
-public class StrokeRookRule {
+import mobi.mpk.chess.domain.Cell;
+import mobi.mpk.chess.domain.Stroke;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StrokeRookRule implements StrokeFigureRule {
+
+    @Override
+    public boolean strokeFigureRule(Stroke stroke) {
+
+        Cell from = stroke.getFrom();
+        Cell to = stroke.getTo();
+
+        boolean isStroke = true;
+
+        if(from.getX()-to.getX()>0 && from.getY() == to.getY()){ // left
+
+            return isStroke;
+
+        } else if(from.getX()-to.getX()<0 && from.getY() == to.getY()){ // right
+
+            return isStroke;
+
+        } else if(from.getY()-to.getY()>0 && from.getX() == to.getX()){ // down
+
+            return isStroke;
+
+        } else if(from.getY()-to.getY()<0 && from.getX() == to.getX()){ // up
+
+            return isStroke;
+
+        }
+
+        isStroke = false;
+        return isStroke;
+
+    }
 
 }
