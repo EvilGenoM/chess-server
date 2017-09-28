a522975f3e1142635494890e685372ab15909a94
EvilGenoM <evmezentsev@gmail.com>
2017-09-28 18:13:38 +0300

    Add rule stroke for Pawn

--- src/main/java/mobi/mpk/chess/domain/rules/rule/stoke/figure/StrokePawnRule.java
+++ src/main/java/mobi/mpk/chess/domain/rules/rule/stoke/figure/StrokePawnRule.java
@@ -1,4 +1,83 @@
 package mobi.mpk.chess.domain.rules.rule.stoke.figure;
 
-public class StrokePawnRule {
+import mobi.mpk.chess.domain.Cell;
+import mobi.mpk.chess.domain.Color;
+import mobi.mpk.chess.domain.Stroke;
+import mobi.mpk.chess.domain.figure.Figure;
+
+public class StrokePawnRule implements StrokeFigureRule {
+
+    @Override
+    public boolean strokeFigureRule(Stroke stroke) {
+
+        Cell from = stroke.getFrom();
+        Cell to = stroke.getTo();
+        Figure figure = stroke.getFigure();
+
+        if (figure.getColor() == Color.white) {
+            return isStrokeWhitePawn(from, to);
+        } else {
+            return isStrokeBlackPawn(from, to);
+        }
+
+    }
+
+    private boolean isStrokeWhitePawn(Cell from, Cell to) {
+
+        boolean isStroke = true;
+
+        if (to.getY() - from.getY() == 1) { // 1 cell
+
+            return isStroke;
+
+        } else if (to.getY() - from.getY() == 2 && from.getY() == 2) { // 2 cell
+
+            return isStroke;
+
+        } else if (Math.abs(to.getY() - from.getY()) == Math.abs(to.getX() - from.getX())) { //attack
+            if (to.getX() - from.getX() == 1 && to.getY() - from.getY() == 1) { //right
+
+                return isStroke;
+
+            } else if (to.getX() - from.getX() == -1 && to.getY() - from.getY() == 1) { //left
+
+                return isStroke;
+
+            }
+        }
+
+        isStroke = false;
+        return isStroke;
+
+    }
+
+    private boolean isStrokeBlackPawn(Cell from, Cell to) {
+
+        boolean isStroke = true;
+
+        if (to.getY() - from.getY() == -1) { // 1 cell
+
+            return isStroke;
+
+        } else if (to.getY() - from.getY() == -2 && from.getY() == 7) { // 2 cell
+
+            return isStroke;
+
+        } else if (Math.abs(to.getY() - from.getY()) == Math.abs(to.getX() - from.getX())) { //attack
+
+            if (to.getX() - from.getX() == 1 && to.getY() - from.getY() == -1) { //right
+
+                return isStroke;
+
+            } else if (to.getX() - from.getX() == -1 && to.getY() - from.getY() == -1) { //left
+
+                    return isStroke;
+
+            }
+        }
+
+        isStroke = false;
+        return isStroke;
+    }
+
 }
