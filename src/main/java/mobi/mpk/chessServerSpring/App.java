ed2e2c23d75e78b1e93334fdf577bf4202431792
EvilGenoM <evmezentsev@gmail.com>
2017-10-26 12:13:27 +0300

    Удалены изменения связанные с регистрацией

--- src/main/java/mobi/mpk/chessServerSpring/App.java
+++ src/main/java/mobi/mpk/chessServerSpring/App.java
@@ -4,12 +4,7 @@ import mobi.mpk.chessServerSpring.registry.UserRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 
 @EnableAutoConfiguration
 @SpringBootApplication
